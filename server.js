e28491bab5a3404cd81514b768898542ab5bcb75
math222ribeiro <math222ribeiro@hotmail.com>
2020-10-18 13:06:31 -0300

    feat: test

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('An alligator approaches!');
 });
 
-app.listen(8080, () => console.log('Gator app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
