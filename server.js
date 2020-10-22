e7abecb30fa8cb4764402e8aba5c5a124ffdeeb1
math222ribeiro <math222ribeiro@hotmail.com>
2020-10-22 00:10:01 -0300

    feat: d

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send('{An alligator approaches!}');
+    res.send('{Hello World!}');
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
